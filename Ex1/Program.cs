c72013f0f29918a87feee1008155aa3382cb620f
dantediy <trandangkha99696@yahoo.com>
2018-03-06 21:40:50 +0700

    Add project files.

---
+++ Ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Dns.GetHostName();
+            IPHostEntry ip = Dns.GetHostByName(name);
+            foreach (IPAddress i in ip.AddressList)
+            {
+                Console.WriteLine(" " + i.ToString());
+            }
+        }
+    }
+}
